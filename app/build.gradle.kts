e5a22183741def837511ef0ca8bdae74be74711f
Mohamed Hesham <m.hesham1851999@gmail.com>
2026-02-18 22:31:58 +0200

    init commit
    init commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -55,4 +56,6 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    // Import the Firebase BoM
+    implementation(platform(libs.firebase.bom))
 }
